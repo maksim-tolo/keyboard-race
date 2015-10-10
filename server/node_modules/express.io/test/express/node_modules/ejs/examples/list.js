e414c9e188666705ebce2d199b15d10efc640dbc
Maxim Tolochko <maksim.tolo@gmail.com>
2015-10-10 11:21:00 +0300

    bug fix

---
+++ server/node_modules/express.io/test/express/node_modules/ejs/examples/list.js
@@ -0,0 +1,14 @@
+
+/**
+ * Module dependencies.
+ */
+
+var ejs = require('../lib/ejs')
+  , fs = require('fs')
+  , str = fs.readFileSync(__dirname + '/list.ejs', 'utf8');
+
+var ret = ejs.render(str, {
+  names: ['foo', 'bar', 'baz']
+});
+
+console.log(ret);
